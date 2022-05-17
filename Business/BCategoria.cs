74f6a583d80ff7b1379ff63d7ff751b5d4c1f0e9
jhonathangiron <jhonathan.giron@tecsup.edu.pe>
2022-05-16 20:51:58 -0500

    Agregar archivos de proyecto.

---
+++ Business/BCategoria.cs
@@ -0,0 +1,79 @@
+﻿using Data;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class BCategoria
+    {
+        private DCategoria dCategoria = null;
+
+        public List<Categoria> Listar(int IdCategoria)
+        {
+            List<Categoria> categorias = null;
+
+
+            dCategoria = new DCategoria();
+            //PROBLEMA
+            categorias = dCategoria.Listar(new Categoria { IdCategoria = IdCategoria });
+
+
+            return categorias;
+        }
+
+        public bool Insertar(Categoria categoria)
+        {
+            bool result = true;
+
+            try
+            {
+                dCategoria = new DCategoria();
+                dCategoria.Insertar(categoria);
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public bool Actualizar(Categoria categoria)
+        {
+            bool result = true;
+
+            try
+            {
+                dCategoria = new DCategoria();
+                dCategoria.Actualizar(categoria);
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public bool Eliminar(int IdCategoria)
+        {
+            bool result = true;
+
+            try
+            {
+                dCategoria = new DCategoria();
+                dCategoria.Eliminar(IdCategoria);
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Data/SQLHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    static class SqlHelper
+    {
+
+
+        public static string Connection { get; } = "Data Source=SQL5059.site4now.net;" +
+                                                   "Initial Catalog=db_a6993a_test;User Id=db_a6993a_test_admin ; Password=Agosto2015.";
+        /// <summary> 
+        /// Set the connection, command, and then execute the command with non query. 
+        /// </summary> 
+        public static Int32 ExecuteNonQuery(String connectionString, String commandText,
+            CommandType commandType, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    // There're three command types: StoredProcedure, Text, TableDirect. The TableDirect  
+                    // type is only for OLE DB.   
+                    cmd.CommandType = commandType;
+                    cmd.Parameters.AddRange(parameters);
+
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        /// <summary> 
+        /// Set the connection, command, and then execute the command and only return one value. 
+        /// </summary> 
+        public static Object ExecuteScalar(String connectionString, String commandText,
+            CommandType commandType, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    cmd.CommandType = commandType;
+                    cmd.Parameters.AddRange(parameters);
+
+                    conn.Open();
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        /// <summary> 
+        /// Set the connection, command, and then execute the command with query and return the reader. 
+        /// </summary> 
+        public static SqlDataReader ExecuteReader(String connectionString, String commandText,
+            CommandType commandType, params SqlParameter[] parameters)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            using (SqlCommand cmd = new SqlCommand(commandText, conn))
+            {
+                cmd.CommandType = commandType;
+                cmd.Parameters.AddRange(parameters);
+
+                conn.Open();
+                // When using CommandBehavior.CloseConnection, the connection will be closed when the  
+                // IDataReader is closed. 
+                SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                return reader;
+            }
+        }
+    }
+}
+
